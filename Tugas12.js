cbcf70dab760916451a1594d7f2a8e345b213e8a
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2019-12-30 04:54:57 +0300

    Tugas 12
    Tugas ini untuk Niomic

---
+++ Tugas12.js
@@ -0,0 +1,13 @@
+function fruit() {
+var fruits = ['pisang', 'jeruk', 'apel', 'mangga'];
+
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+
+fruits.shift()
+console.log(fruits)
+}
+
+fruit()
